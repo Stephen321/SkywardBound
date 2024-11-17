8a5d24d1db51144ffab12683b3cc02b132f3bcdd
Stephen321 <Stephen321@users.noreply.github.com>
2024-11-17 11:15:16 +0000

    moved files to separate folders, gravity only on player

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,9 +4,10 @@ extends Area2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const UPWARD_MOMENTUM = -100
+const GRAVITY_SCALED = Vector2(0, 9.81) * 50
 
 var jump: bool = false
-var velocity: Vector2 = Vector2(0, UPWARD_MOMENTUM)
+var velocity: Vector2
 var acceleration: Vector2
 
 func _input(event: InputEvent) -> void:
@@ -14,11 +15,8 @@ func _input(event: InputEvent) -> void:
 		jump = true
 
 func _physics_process(delta: float) -> void:
-	acceleration = Vector2(0, UPWARD_MOMENTUM)
+	acceleration = GRAVITY_SCALED
 	velocity += acceleration * delta
-	if jump:
-		velocity.x = UPWARD_MOMENTUM * 80 * delta
-		velocity.y = UPWARD_MOMENTUM * 150 * delta
-		jump = false
 	position += (velocity * delta)
-	pass
+	print("asdasd hi velocity %s" % velocity)
+	print("position %s" % position)
